f6f9ae2f983fe301e7c1d54ef36bc0f23ae97c1b
DmGsOuNaK <sounak.swamiji@gmail.com>
2023-06-16 13:33:44 +0530

    Updates

---
+++ precache-manifest.22133c5bf916afffd7cac7e806c6a731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81a68b318c226813e3ea",
+    "url": "/age-calculator/static/css/main.d726e4bf.chunk.css"
+  },
+  {
+    "revision": "81a68b318c226813e3ea",
+    "url": "/age-calculator/static/js/main.81a68b31.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/age-calculator/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/age-calculator/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "762589126f838bd6fab8",
+    "url": "/age-calculator/static/js/runtime~main.76258912.js"
+  },
+  {
+    "revision": "2b3ddcbadf38be41c466576653a81e07",
+    "url": "/age-calculator/index.html"
+  }
+];
